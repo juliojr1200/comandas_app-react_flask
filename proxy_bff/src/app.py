5882cfb07e123202870730269b09cadbdbf751da
Julio <jpasteuzinho@gmail.com>
2025-05-12 20:22:33 -0300

    pt2 proxy

--- proxy_bff/src/app.py
+++ proxy_bff/src/app.py
@@ -2,13 +2,15 @@ import os
 from flask import Flask, send_from_directory, session
 from datetime import timedelta
 import logging
-# carrega o arquivo .env, variáveis de ambiente
-from settings import PROXY_PORT, PROXY_DEBUG, TEMPO_SESSION
-
+from settings import PROXY_PORT, PROXY_DEBUG, TEMPO_SESSION  # carrega o arquivo .env, variáveis de ambiente
 from funcoes import Funcoes
+
+# blueprints
+from mod_funcionario.funcionario import bp_funcionario
 
 # Configuração básica de logging
 logging.basicConfig(level=logging.INFO)
+
 # Aplicação Flask
 app = Flask(__name__)
 
@@ -28,10 +30,35 @@ def favicon():
 def teste_token():
     return Funcoes.get_api_token()
 
-# código omitido
+# gerando uma chave randômica para secret_key
+app.secret_key = os.urandom(12).hex()
+
+# configuração do tempo de expiração da sessão (em minutos), o padrão é 31 dias
+app.permanent_session_lifetime = timedelta(minutes=int(TEMPO_SESSION))
+
+# Configuração do SameSite para cookies
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+
+# Configuração para enviar cookies apenas em conexões HTTPS
+app.config['SESSION_COOKIE_SECURE'] = True
+
+# o decorador @app.before_request é chamado antes de cada requisição
+@app.before_request
+def before_request():
+    # renovar o tempo da sessão automaticamente conforme o usuário interage com a aplicação
+    session.permanent = True
+
+# registra as rotas dos blueprints
+app.register_blueprint(bp_funcionario)
+
 # ponto de entrada para execução
 if __name__ == '__main__':
     logging.info(f"Iniciando o servidor Flask na porta: {PROXY_PORT}")
     # Roda o servidor Flask em modo de debug (recarrega automaticamente e mostra mais erros)
     # Desative o debug em produção!
-    app.run(host='0.0.0.0', port=PROXY_PORT, debug=PROXY_DEBUG, use_reloader=PROXY_DEBUG)
+    app.run(
+        host='0.0.0.0',
+        port=PROXY_PORT,
+        debug=PROXY_DEBUG,
+        use_reloader=PROXY_DEBUG
+    )
--- proxy_bff/src/funcoes.py
+++ proxy_bff/src/funcoes.py
@@ -4,6 +4,7 @@ import requests
 from settings import API_ENDPOINT_TOKEN, API_USERNAME_TOKEN, API_PASSWORD_TOKEN, API_SSL_VERIFY
 import logging
 
+
 class Funcoes(object):
     # função para obter o token da API externa, retorna o json do token obtido ou do erro - os dados do token são armazenados na sessão do Flask para uso posterior
     @staticmethod
@@ -58,4 +59,57 @@ class Funcoes(object):
             logging.error(msg)
 
             # retornar json com chave de erro e mensagem de erro
+            return {'error': msg}, 500
+
+    # valida o token armazenado na sessão. Caso esteja expirado ou não exista, faz até duas tentativas para obter um novo.
+    @staticmethod
+    def validar_token():
+        for _ in range(2):  # Tenta obter o token no máximo 2 vezes
+            if 'token_validade' in session and session['token_validade'] > datetime.timestamp(datetime.now()):
+                # Token válido
+                return True
+            # Token inválido ou expirado, tenta obter um novo
+            if 'access_token' in Funcoes.get_api_token():
+                return True  # Novo token obtido com sucesso
+        # Se chegar aqui, significa que não foi possível obter um token válido
+        return False
+
+    @staticmethod
+    def make_api_request(method, url, data=None, params=None):
+        # verifica se tem um token dentro da validade
+        if Funcoes.validar_token() == False:
+            return {'error': 'Falha ao obter token de autenticação'}, 500
+
+        # monta o cabeçalho da requisição, com o token obtido
+        headers = {
+            'Authorization': f'Bearer {session["access_token"]}',
+            'accept': 'application/json',
+        }
+
+        try:
+            logging.info(f"Realizando requisição: {method.upper()} {url}")
+
+            # realiza o request na API externa, utilizando o método, url, cabeçalho, dados e parâmetros fornecidos
+            response = requests.request(method, url, headers=headers, json=data, params=params, verify=API_SSL_VERIFY)
+
+            # quando a requisição é bem-sucedida (status 200-299): O método não faz nada e o código continua normalmente.
+            # quando a requisição falha (status fora de 200-299): Ele lança uma exceção requests.exceptions.HTTPError.
+            response.raise_for_status()
+
+            # monta o json com os dados retornados
+            result = response.json()
+
+            # nossa a api retorna um array json, no qual o primeiro elemento é o resultado e o segundo elemento é o status_code
+            return result[0] if response.content else {}, response.status_code
+
+        except Exception as e:
+            msg = f"Erro inesperado ao processar requisição para API externa: {e}"
+
+            # Se a exceção for do tipo HTTPError, pode-se acessar o código de status e a mensagem de erro
+            if isinstance(e, requests.exceptions.HTTPError):
+                msg = f"Erro HTTP: {e.response.status_code} - {e.response.text}"
+            elif isinstance(e, requests.exceptions.RequestException):
+                msg = f"Erro de conexão/requisição com a API externa: {e}"
+
+            logging.error(msg)
             return {'error': msg}, 500
